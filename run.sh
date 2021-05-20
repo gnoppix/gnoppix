53ed4461f421eff6da9efdac997d37184b8d91d6
“gnoppix” <amu@sihanan.com>
2021-05-20 21:16:04 +0700

    update

---
+++ run.sh
@@ -0,0 +1,20 @@
+echo ""
+echo "
+
+export WORK=~/work
+export CD=~/cd
+export FORMAT=squashfs
+export FS_DIR=casper
+
+echo ""
+echo "Make sure we have all Directoories" 
+echo ""
+sudo mkdir -p ${CD}/{${FS_DIR},boot/grub} ${WORK}/rootfs
+echo ""
+echo "" 
+
+
+
+
+
+
